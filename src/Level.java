ceb9aabd7ae53f235eb9c14a5bdc33b4255d8048
feynman <feynmanz@nycstudents.net>
2025-10-06 02:16:07 -0400

    finished

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Level {
+    private boolean goalReached;
+    private int points;
+    public boolean goalReached()
+    {
+        return goalReached;
+    }
+    public void reachGoal()
+    {
+        goalReached = true;
+    }
+    public int getPoints()
+    {
+        return points;
+    }
+    public void setPoints(int p)
+    {
+        points = p;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Game one = new Game();
+        one.getLevel(1).reachGoal();
+        one.getLevel(1).setPoints(200);
+        one.getLevel(2).reachGoal();
+        one.getLevel(2).setPoints(100);
+        one.getLevel(3).reachGoal();
+        one.getLevel(3).setPoints(500);
+        one.makeBonus();
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        System.out.println(one.getScore());
+
+        Game two = new Game();
+        two.getLevel(1).reachGoal();
+        two.getLevel(1).setPoints(200);
+        two.getLevel(2).reachGoal();
+        two.getLevel(2).setPoints(100);
+        two.getLevel(3).setPoints(500);
+        System.out.println(two.getScore());
+
+        Game three = new Game();
+        three.getLevel(1).reachGoal();
+        three.getLevel(1).setPoints(200);
+        three.getLevel(2).setPoints(100);
+        three.getLevel(3).reachGoal();
+        three.getLevel(3).setPoints(500);
+        three.makeBonus();
+        System.out.println(three.getScore());
+
+        Game four = new Game();
+        four.getLevel(1).setPoints(200);
+        four.getLevel(2).reachGoal();
+        four.getLevel(2).setPoints(100);
+        four.getLevel(3).reachGoal();
+        four.getLevel(3).setPoints(500);
+        System.out.println(four.getScore());
+
+        Game five = new Game();
+        System.out.println(five.playManyTimes(4));
+    }
+
 }
